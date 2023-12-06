451e9aef2b68ad99908112bad3cad83fff748ced
rohitmauryax <rohitmaurya152@gmail.com>
2023-12-06 16:36:05 +0530

    first

--- backend/Routes/SearchData.js
+++ backend/Routes/SearchData.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const notes = require("../modal/Notes");
+const video = require("../modal/video");
 
 router.get("/query", async (req, res) => {
   const { search } = req.query;
@@ -8,6 +9,15 @@ router.get("/query", async (req, res) => {
     // const filter = { tag: search };
     // res.send(filter);
     const data = await notes.find({ tag: search }).exec();
+    res.send(data);
+  }
+});
+router.get("/playlist/query", async (req, res) => {
+  const { tag } = req.query;
+  if (tag) {
+    // const filter = { tag: search };
+    // res.send(filter);
+    const data = await video.find({ tag: tag }).exec();
     res.send(data);
   }
 });
