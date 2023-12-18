5f0e4a1f4fee60abd58a082b4f65849f1e652917
rohitmauryax <rohitmaurya152@gmail.com>
2023-12-19 01:41:03 +0530

    load more

--- src/components/Pages/Video.jsx
+++ src/components/Pages/Video.jsx
@@ -11,14 +11,26 @@ const Video = () => {
   const video = useFetch(FINAL_API);
   const videoList = video.items;
   console.log(videoList);
-
+  const [item, setItem] = useState(8);
   return videoList === undefined || videoList.length === 0 ? (
     <Shimmer />
   ) : (
-    <div className="flex justify-around content-evenly gap-2 flex-wrap my-4 ">
-      {videoList.map((video, index) => {
-        return <VideoCards key={index} video={video} />;
-      })}
+    <div>
+      <div className="flex justify-around content-evenly gap-2 flex-wrap my-4 ">
+        {videoList.slice(0, item).map((video, index) => {
+          return <VideoCards key={index} video={video} serial={index} />;
+        })}
+      </div>
+      {item <= videoList.length && (
+        <div className="flex justify-center m-10">
+          <button
+            className="bg-blue-500 text-white p-2 rounded-md shadow-md shadow-slate-600 hover:bg-blue-600 hover:shadow-lg"
+            onClick={() => setItem(item + 8)}
+          >
+            Load More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Pages/VideoCards.jsx
+++ src/components/Pages/VideoCards.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const VideoCards = ({ video }) => {
+const VideoCards = ({ video, serial }) => {
   const { title, description, thumbnails } = video.snippet;
   return (
     <>
       <Link to={"/videoplayer"} state={video}>
         <div className="flex flex-col justify-between rounded-sm h-[420px] w-[350px] cursor-pointer shadow-slate-500 shadow-sm transition-all hover:shadow-lg ">
+          <h1 className="absolute text-cyan-300 mx-1 ">{serial + 1}</h1>
           <img src={thumbnails.high.url} alt="" />
           <h1 className="font-medium font-rob text-lg p-2">{title}</h1>
           <div className="">
